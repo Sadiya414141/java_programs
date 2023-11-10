6aca7879a415dea07b7e1d7133f9b15af138a372
Sadiya414141 <102041355+Sadiya414141@users.noreply.github.com>
2023-11-10 10:33:08 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+class Test
+{
+    public static void main(String []args)
+    {
+         int num1 = 5;
+         int num2 = 20;
+        System.out.println(num1+ num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    
+        
+    }
+};
